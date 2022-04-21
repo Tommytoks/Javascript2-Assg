7dbf239cd97491097bde0a37b7916f56e383a121
Tommytoks <adaranijotomilola@gmail.com>
2022-04-21 12:00:34 +0100

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 function lifeInWeeks(age) {
-    var age = parseInt(prompt('Input your current age'));
-    var years = 90 - age;
+    var age = parseInt(prompt('Input your current age')); //29
+    const years = 90 - age;  //61
     var Sam = years * 365;
     var Tew = years * 52;
     var Tom = years * 12;
-  
     var output = "You have " + Sam + " days, " + Tew + " weeks " + "and " + Tom + " months left if you live until 90 years old.";
-    console.log(output)
+    
+    return output;
 }
-lifeInWeeks();
+var my_age = lifeInWeeks();
+alert(my_age)
 
